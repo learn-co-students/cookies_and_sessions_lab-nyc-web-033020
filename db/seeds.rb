d24747d6ba61019e3192a2ca7f3cd101dfef169c
mishyjari <mishyjari@outlook.com>
2020-05-03 10:35:22 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.destroy_all
+
+Product.create(name: 'Walnuts')
+Product.create(name: 'Avocado')
+Product.create(name: 'Tomatos')
+Product.create(name: 'Paprika')
+Product.create(name: 'Garlic')
+Product.create(name: 'Lox')
+Product.create(name: 'Milk')
+Product.create(name: 'Pickles')
+Product.create(name: 'Butter')
