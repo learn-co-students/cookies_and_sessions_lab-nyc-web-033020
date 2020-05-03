b05b1949400c324df2fb0540589110c19c89f04b
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-03 14:40:25 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  get '/', to: 'products#index'
+
+  post '/add', to: 'products#add'
+
+  # root 'products#index'
+  # post '/' => 'products#add'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
